5b70b5966811908251eae374b4a961afa7b23e49
ybai62868 <ybai62868@gmail.com>
2020-09-30 20:06:41 +0800

    [Add] IndexCalcuation

---
+++ tutorials/03-IndexCalculation/main.cu
@@ -0,0 +1,57 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <cstdio>
+#include <cstdlib>
+
+__global__ void unique_idx_calc_threadIdx(int* input)
+{
+  int tid = threadIdx.x;
+  printf("threadIdx : %d, value: %d \n", tid, input[tid]);
+
+}
+
+__global__ void unique_gid_calculation(int* input)
+{
+  int tid = threadIdx.x;
+  int offset = blockIdx.x * blockDim.x;
+  int gid = tid + offset;
+  printf("blockIdx.x : %d, threadIdx.x : %d, grid : %d, value : %d\n", blockIdx.x,
+          threadIdx.x, gid, input[gid]);
+
+}
+  
+
+
+int main(void)
+{
+  // int array_size = 8;
+  int array_size = 16;
+  int array_byte_size = sizeof(int) * array_size;
+  // int h_data[] = {23, 9, 4, 53, 65, 12, 1, 33};
+  int h_data[] = {23, 9, 4, 53, 65, 12, 1, 33, 1, 6, 2, 2, 6, 8, 6, 10};
+
+  for ( int i = 0;i < array_size;i++ ) {
+    printf("%d ", h_data[i]);
+  }
+  printf("\n \n");
+
+  int* d_data;
+  cudaMalloc((void**)&d_data, array_byte_size);
+  cudaMemcpy(d_data, h_data, array_byte_size, cudaMemcpyHostToDevice);
+
+  //dim3 block(8);
+  //dim3 grid(1);
+
+  dim3 block(4);
+  dim3 grid(4);
+
+
+  // unique_idx_calc_threadIdx<<<grid, block>>>(d_data);
+  unique_gid_calculation<<<grid, block>>>(d_data);
+  cudaDeviceSynchronize();
+
+  cudaDeviceReset();
+
+  return 0;
+}
